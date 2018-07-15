93991ec2cfe28f937250bae84a72d4c67742bf22
e_ntyo <entyo.contact@gmail.com>
2018-07-15 21:35:15 +0900

    ジャイロでジャイジャイ

---
+++ Assets/ControlHand.cs
@@ -0,0 +1,18 @@
+﻿public class ControlHand : UnityEngine.MonoBehaviour
+{
+    private UnityEngine.Gyroscope gyro;
+
+    // Use this for initialization
+    void Start()
+    {
+        gyro = UnityEngine.Input.gyro;
+        gyro.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = new UnityEngine.Quaternion(gyro.attitude.x, 0, gyro.attitude.y, 1);
+        UnityEngine.Debug.Log(gyro.attitude);
+    }
+}
