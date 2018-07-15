c6caadc484240c7fdeae21a1c06bab815801d2d2
e_ntyo <entyo.contact@gmail.com>
2018-07-15 23:38:29 +0900

    タイトル画面をつくる

---
+++ Assets/SceneTransition.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;//シーンマネジメントを有効にする
+
+public class SceneTransition : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		// 左クリック or タップ
+        if (Input.GetMouseButtonDown(0) || Input.touches.Length > 0)
+        {
+            SceneManager.LoadScene("Game");
+        }
+
+    }
+}
